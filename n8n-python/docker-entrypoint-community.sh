2ea7cd0f518fbccb23a542ceff0f437cc9991387
Leonardo Sete <leonardo_o_sete_lab2dev@whirlpool.com>
2025-04-23 16:04:30 -0300

    feat: n8n com python - teste 33

--- n8n-python/docker-entrypoint-community.sh
+++ n8n-python/docker-entrypoint-community.sh
@@ -1,22 +1,18 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh          # ← use sh, não bash
 set -e
 
-# diretório do PVC
 NODES_DIR="/home/node/.n8n/nodes"
-
-# garante que a pasta exista no PVC
 mkdir -p "$NODES_DIR"
 
-# ativa pnpm (caso Corepack ainda não tenha baixado binário)
+# habilita pnpm
 corepack enable && corepack prepare pnpm@latest --activate
 
-# instala apenas se ainda não houver node_modules
 if [ ! -d "$NODES_DIR/node_modules" ]; then
-  echo "[bootstrap] Instalando n8n-nodes-evolution-api e n8n-nodes-python..."
+  echo "[bootstrap] instalando community-nodes"
   pnpm add --dir "$NODES_DIR" \
     --allow-scripts=n8n-nodes-evolution-api,n8n-nodes-python \
     n8n-nodes-evolution-api n8n-nodes-python
 fi
 
-# entrega para o entrypoint original do n8n
+# chama o entrypoint original
 exec /docker-entrypoint.sh "$@"
