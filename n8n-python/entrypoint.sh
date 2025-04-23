e1526d0858bc7da1e830b8c9f3117ede0301f632
Leonardo Sete <leonardo_o_sete_lab2dev@whirlpool.com>
2025-04-23 13:39:24 -0300

    feat: n8n com python - teste 20

--- n8n-python/entrypoint.sh
+++ n8n-python/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
-# roda como root ainda em cima do n8n:1.89.2
-pip install fire --break-system-packages
-# teste - agora executa o comando padrão do container
+# entrypoint.sh
+
+# Aqui você pode colocar comandos adicionais que precisar rodar
+# antes de iniciar o n8n (por exemplo, migrações, hooks, etc).
+
+# Finalmente, executa o comando padrão (n8n start)
 exec "$@"
