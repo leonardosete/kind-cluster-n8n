1af985240ea1e03c56bcc789a104348c8d72537e
Leonardo Sete <leonardo_o_sete_lab2dev@whirlpool.com>
2025-04-23 15:34:28 -0300

    feat: n8n com python - teste 29

--- n8n-python/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-# entrypoint.sh
-
-# Aqui você pode colocar comandos adicionais que precisar rodar
-# antes de iniciar o n8n (por exemplo, migrações, hooks, etc).
-
-# Finalmente, executa o comando padrão (n8n start)
-exec "$@"
